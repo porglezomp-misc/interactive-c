3491af7cce3d87a6103dfa0696a3669fb6a4b588
Caleb Jones <self@calebjones.net>
2016-03-25 23:28:50 -0400

    Reload the dynamic library every iteration

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include <dlfcn.h>
 
 #include "state.h"
@@ -10,12 +12,42 @@ const char *libfile = "libstateful.so";
 state_api STATE_API;
 void *wrapper_malloc(int size) { return calloc(1, size); }
 
+static bool first_load = true;
+static volatile bool should_reload = true;
+static bool running = true;
+
 int main() {
-  void *library = dlopen(libfile, RTLD_LAZY);
-  state_api *api = dlsym(library, "STATE_API");
-  memcpy(&STATE_API, api, sizeof(STATE_API));
-  struct state *s = STATE_API.init(wrapper_malloc);
-  STATE_API.step(s);
+  void *library = NULL;
+  state_api *api = NULL;
+  struct state *s = NULL;
+
+  while (running) {
+    should_reload = true;
+    if (should_reload) {
+      should_reload = false;
+      if (library) {
+        dlclose(library);
+      }
+      library = dlopen(libfile, RTLD_LAZY);
+      if (!library) {
+        printf("Error opening library: %s\n", dlerror());
+        continue;
+      }
+      api = dlsym(library, "STATE_API");
+      if (!api) {
+        printf("error: %s\n", dlerror());
+        continue;
+      }
+      memcpy(&STATE_API, api, sizeof(STATE_API));
+      if (first_load) {
+        first_load = false;
+        s = STATE_API.init(wrapper_malloc);
+      }
+    }
+    STATE_API.step(s);
+    sleep(1);
+  }
   STATE_API.finalize(s);
   return 0;
 }
+
